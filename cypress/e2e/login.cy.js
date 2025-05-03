86e8a8ad480dd121831d442c26ebc6164b134b00
Dhimas <satriyowibowo559@gmail.com>
2025-05-03 21:34:50 +0700

    fix cypress

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -54,7 +54,7 @@ describe('Login spec', () => {
 
     cy.get('button').contains(/^Login$/).click();
 
-    cy.get('.navigation').should('be.visible');
+    cy.get('.navigation__container').should('be.visible');
     cy.get('.btn-signout').should('be.visible');
   });
 });
